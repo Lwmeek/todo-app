9adc5977ae8d91ef9f89c25dde9eaad19759722e
Timic Ivey <timicivey27@gmail.com>
2023-11-30 12:28:37 -0500

    Add footer and header content in

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+	return <p>Drag and drop to reorder list</p>;
+};
+
+export default Footer;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<div>
+			<img src="./src/images/bg-mobile-light.jpg" alt="" />
+            <h1 className="font-bold">TODO </h1>
+            <img src="./src/images/icon-moon.svg" alt="" />
+		</div>
+	);
+};
 
-export default Header
+export default Header;
