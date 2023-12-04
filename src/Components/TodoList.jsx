e96fabcb88dca6cd40523a6276f03b730ce98bb4
Timic Ivey <timicivey27@gmail.com>
2023-12-04 11:42:47 -0500

    start making task list with a default display

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Checkbox } from "@nextui-org/react";
+import { Checkbox, CheckboxGroup } from "@nextui-org/react";
 
 const TodoList = () => {
 	const [active, setActive] = useState(0);
@@ -8,9 +8,50 @@ const TodoList = () => {
 	};
 	return (
 		<div className="flex gap-4">
-			<Checkbox lineThrough radius="full" size="lg">
-				Create a new todo...
-			</Checkbox>
+			<CheckboxGroup
+				className="tasks ml-3 rounded-lg shadow-[1px_15px_20px_var(--clr-Light-Grayish-Blue-1)]"
+				label="Create a new todo..."
+			>
+				<div className="flex justify-between items-center tasks-item">
+					<Checkbox lineThrough radius="full" size="lg">
+						<p className="ml-2">Complete Javascript course</p>
+					</Checkbox>
+					<img src="./src/images/icon-cross.svg" alt="" />
+				</div>
+				<div className="flex justify-between items-center tasks-item">
+					<Checkbox value="2" lineThrough radius="full" size="lg">
+						Jog around the park 3x
+					</Checkbox>
+					<img src="./src/images/icon-cross.svg" alt="" />
+				</div>
+				<div className="flex justify-between items-center tasks-item">
+					<Checkbox value="3" lineThrough radius="full" size="lg">
+						10 minutes meditation
+					</Checkbox>
+					<img src="./src/images/icon-cross.svg" alt="" />
+				</div>
+				<div className="flex justify-between items-center tasks-item">
+					<Checkbox value="4" lineThrough radius="full" size="lg">
+						Read for 1 hour
+					</Checkbox>
+					<img src="./src/images/icon-cross.svg" alt="" />
+				</div>
+				<div className="flex justify-between items-center tasks-item">
+					<Checkbox value="5" lineThrough radius="full" size="lg">
+						Complete Todo App on Frontend Mentor
+					</Checkbox>
+					<img src="./src/images/icon-cross.svg" alt="" />
+				</div>
+				<div className="flex justify-between">
+					<p>5 items left</p>
+					<button>Clear Completed</button>
+				</div>
+			</CheckboxGroup>
+			<div className="flex flex-col">
+				<Checkbox lineThrough radius="full" size="lg">
+					Create a new todo...
+				</Checkbox>
+			</div>
 			<div className="tabs">
 				<ul>
 					<li
